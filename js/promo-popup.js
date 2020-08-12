820cdddbe80b2076d9d8785bcd582b32ec025167
michalbreakthru <69454759+michalbreakthru@users.noreply.github.com>
2020-08-12 12:09:37 +0200

    translate

--- js/promo-popup.js
+++ js/promo-popup.js
@@ -1,39 +1,56 @@
 $(document).ready(function($) {
-	
-function NewPopUp(){
+
+var language = window.navigator ? (window.navigator.language ||
+                  window.navigator.systemLanguage ||
+                  window.navigator.userLanguage) : "en";
+language = language.substr(0, 2).toLowerCase();
+console.log(language);
+
+var textpc = "Would you like to join our team and become the </br> painting animator?"; 
+var clickpc = "Click here"; 
+var urlpc = "https://join.breakthrufilms.pl/en/"; 
+var closephone = "Close"; 
+
+if(language=="pl"){
+var textpc = "Chcesz dołączyć do naszego zespołu i zostać </br>animatorem?"; 
+var clickpc = "Tak"; 
+var urlpc = "https://join.breakthrufilms.pl/pl/"; 
+var closephone = "Nie"; 
+}
+
+
+
+
+if (Cookies.get('popup_status') != '1'){
 var width = $(document).width();
 console.log(width);
 if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)) {
+document.getElementById("textphone").innerHTML = textpc;
+document.getElementById("urlphone").setAttribute('href', urlpc);
+document.getElementById("clickphone").setAttribute('value', clickpc);
+document.getElementById("closephone").setAttribute('value', closephone);
 
 $('.popup-fade-phone').fadeIn();
-
 	$('.popup-close-phone').click(function() {
 		$(this).parents('.popup-fade-phone').fadeOut();
 		return false;
 	});		
-
- 
-
 	$(document).keydown(function(e) {
 		if (e.keyCode === 27) {
 			e.stopPropagation();
 			$('.popup-fade-phone').fadeOut();
 		}
 	});
-
-	
-
 	$('.popup-fade-phone').click(function(e) {
 		if ($(e.target).closest('.popup-phone').length == 0) {
 			$(this).fadeOut();					
 		}
 	});
-
-
 } 
 else{
-
-
+document.getElementById("textpc").innerHTML = textpc;
+document.getElementById("clickpc").innerHTML = clickpc;
+document.getElementById("urlpc").setAttribute('href', urlpc);
 $('.popup-fade').fadeIn();
 
 	$('.popup-close').click(function() {
@@ -62,9 +79,7 @@ $('.popup-fade').fadeIn();
 }
 }
 	
-	if (Cookies.get('popup_status') != '1') {
-	NewPopUp();
-	}
+	
 	
 	Cookies.set('popup_status', '1', {
     expires: 60
